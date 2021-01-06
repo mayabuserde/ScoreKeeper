8dae736697311b470e30da8b21918af2b8a59f99
Maya Buser De <maya.buser@gmail.com>
2021-01-06 17:00:34 -0500

    updated version with code along

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const scoreCounter = document.querySelector('#scoreCounter');
+
 const maxScore = document.querySelector('#maxScore');
 const player1 = document.querySelector('#player1');
 const player2 = document.querySelector('#player2');
@@ -7,56 +7,58 @@ const player2Score = document.querySelector('#player2Score');
 const reset = document.querySelector('#reset');
 let player1Count = 0;
 let player2Count = 0;
+let winningScore = 5;
+let isGameOver = false;
 
-
-// getMaxScore = function (e) {
-//     let neededToWin = e.target.value;
-//     return neededToWin;
-// }
-// let neededToWin = maxScore.addEventListener('change', getMaxScore);
-// console.log(neededToWin);
-
-player1.addEventListener('click', function () {
-    player1Count++;
-
-
-
-    player1Score.innerText = player1Count;
-
-
-    if (player1Count === 5) {
-        player1Score.classList.add('win');
-        player2Score.classList.add('lose');
-
-
-
-    }
-
-})
-
-player2.addEventListener('click', function () {
-    player2Count++;
-
-    player2Score.innerText = player2Count;
-
-
-    if (player2Count === 5) {
-        player2Score.classList.add('win');
-        player1Score.classList.add('lose');
-
-    }
-
-
-
-})
-
-reset.addEventListener('click', function () {
+resetGame = function () {
     player1Count = 0;
     player2Count = 0;
     player1Score.innerText = player1Count;
     player2Score.innerText = player2Count;
     player1Score.classList.remove('win', 'lose');
     player2Score.classList.remove('win', 'lose');
+    isGameOver = false;
+
+}
+
+//update winning score and reset game
+maxScore.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    resetGame();
+})
+
+
+player1.addEventListener('click', function () {
+    if (!isGameOver) {
+        player1Count++;
+    }
+
+    if (player1Count === winningScore) {
+        player1Score.classList.add('win');
+        player2Score.classList.add('lose');
+        isGameOver = true;
+    } player1Score.innerText = player1Count;
 
 })
+
+player2.addEventListener('click', function () {
+    if (!isGameOver) {
+        player2Count++;
+
+    }
+
+    if (player2Count === winningScore) {
+        player2Score.classList.add('win');
+        player1Score.classList.add('lose');
+        isGameOver = true;
+
+    }
+
+    player2Score.innerText = player2Count;
+
+})
+
+reset.addEventListener('click', resetGame);
+
+
 
